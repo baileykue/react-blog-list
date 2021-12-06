67d7dd392dd8cf6155e4eb531a98808493db223c
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-06 14:32:41 -0800

    map through the blogs data to populate the page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
 import { getBlogs } from './services/blogs';
+import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      //important to await this data because when we get the data is it coming from an async function
       const data = await getBlogs();
       setBlogs(data);
     };
@@ -19,7 +19,9 @@ function App() {
   return (
     <section className="main">
       <Header />
-
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
       <Footer />
     </section>
   );
